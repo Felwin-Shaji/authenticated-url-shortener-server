a02aaf2148f85022b2f9f0b20c4ab6146da8be5f
Felwin Shaji <felwinshaji@gmail.com>
2026-08-19 08:10:10 +0530

    first commit with next implimented and db connnected

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    })
+  );
+
+  app.enableCors();
+
+  console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  console.log(app);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
